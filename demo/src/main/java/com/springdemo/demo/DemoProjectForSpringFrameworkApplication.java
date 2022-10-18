c5ba55c08ada3807c03002c919da204399a483b7
raji.venkatesh <raji.venkatesh@accomsolutions.com>
2022-10-18 07:53:04 -0500

    first commit

---
+++ demo/src/main/java/com/springdemo/demo/DemoProjectForSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectForSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectForSpringFrameworkApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/springdemo/demo/DemoProjectForSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectForSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
