99f301661323654c0f9c675e078e4e06a6ddf121
raji.venkatesh <raji.venkatesh@accomsolutions.com>
2022-10-21 13:59:57 -0500

    Initial Project Add

---
+++ raji-pet-clinic/src/main/java/com/springframework/rajipetclinic/RajiPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.rajipetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RajiPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajiPetClinicApplication.class, args);
+	}
+
+}
---
+++ raji-pet-clinic/src/test/java/com/springframework/rajipetclinic/RajiPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.rajipetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RajiPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
